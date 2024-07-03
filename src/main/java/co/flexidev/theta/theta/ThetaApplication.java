8657db7d772d9f144b147781ff92b80892fb2a65
Robson Martins <robson.java@gmail.com>
2024-07-03 20:36:44 -0300

    config projet

---
+++ src/main/java/co/flexidev/theta/theta/ThetaApplication.java
@@ -0,0 +1,13 @@
+package co.flexidev.theta.theta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/flexidev/theta/theta/ThetaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.flexidev.theta.theta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
